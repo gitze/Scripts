a532868337112f82f3ff15fdbec7e5f5da23e2fd
gitze <georg@itzenplitz.ch>
2021-01-01 13:57:37 +0100

    Update backup_raspifull.sh

--- RaspiBackup/backup_raspifull.sh
+++ RaspiBackup/backup_raspifull.sh
@@ -2,20 +2,24 @@
 # backup_raspifull.sh
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
+BACKUPDIR=/automnt/backup
+BACKUPNAME=gartenrpi
+
 NOW=$(date +"%Y-%m-%d %H:%M:%S")
-CONTROLFILE=/automnt/backup/BACKUP_USB
+CONTROLFILE=${BACKUPDIR}/BACKUP_USB
 WEEKDAY=`date +%u`
 FILESIZE="4000"
-FILENAME="/automnt/backup/gartenrpi_"$FILESIZE"_backup_"$WEEKDAY".img"
+FILENAME="${BACKUPDIR}/${BACKUPNAME}_"$FILESIZE"_backup_"$WEEKDAY".img"
+
 echo "################################################################################"
 echo "#    RASPI BACKUP"
 echo "#    Date:        $NOW"
 echo "#    File:        $FILENAME"
 echo "################################################################################"
 if [ ! -e "$CONTROLFILE" ]; then
-    echo "BACKUP USB Stick not found." >&2
+    echo "BACKUP Location not found." >&2
     echo "Looking for folowing file: $CONTROLFILE" >&2
     exit 1
 else 
-    /opt/solar/bkup_rpimage.sh start -c -s $FILESIZE "$FILENAME"
+    /opt/raspibackup/bkup_rpimage.sh start -c -s $FILESIZE "$FILENAME"
 fi 

5d8f2f0684e9d413d3294d2366c03c392c1c0a7b
gitze <georg@itzenplitz.ch>
2021-03-25 22:38:46 +0100

    Update editConfig.sh

--- editConfig.sh
+++ editConfig.sh
@@ -4,22 +4,24 @@
 # Delimiter is ":" or "="
 #
 #echo Script name: $0
-#echo $# arguments 
+#echo $# arguments
 
-editConfigKey () {
-    if [ $# -ne 3 ]; 
-    then 
+editConfigKey() {
+    if [ $# -ne 3 ]; then
         echo "illegal number of parameters"
         echo "$0 $@"
-        exit 1        
+        exit 1
     fi
-    configfile=$1; shift
-    key=$1; shift
-    replace=$1; shift
+    configfile=$1
+    shift
+    key=$1
+    shift
+    replace=$1
+    shift
 
     [ ! -e "$configfile" ] && touch $configfile
     sed -E -i .bak "s/^($key[[:blank:]]*[=|:][[:blank:]]*).*/\1$replace/" "$configfile"
-    [ $(grep -c "^$key[[:blank:]]*[=|:]" "$configfile") -eq 0 ] && echo "$key=$replace" >> "$configfile"
+    [ $(grep -c "^$key[[:blank:]]*[=|:]" "$configfile") -eq 0 ] && echo "$key=$replace" >>"$configfile"
 }
 
 editConfigKey "$@"

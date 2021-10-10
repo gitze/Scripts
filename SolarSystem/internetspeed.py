9b818ddbd05e2d1759d5273e7b5cc2f6665e6d53
gitze <georg@itzenplitz.ch>
2021-10-10 20:14:03 +0200

    refactoring

--- SolarSystem/internetspeed.py
+++ SolarSystem/internetspeed.py
@@ -7,20 +7,16 @@ import serial.tools.list_ports
 import sys
 import binascii
 import json
-import requests
-from urllib.parse import urlencode, quote_plus
 import time
 import datetime
 import configparser
-import logging.handlers
+import logging,subprocess
 import solar_logger
-import csv
-import subprocess
+import solar_threadhandler
 
 
 def get_script_path():
     return os.path.dirname(os.path.realpath(sys.argv[0]))
-
 
 # ###########################
 # Config Parameter
@@ -31,78 +27,68 @@ emoncsm_url = "https://url"
 
 config = configparser.ConfigParser()
 config.read(get_script_path()+'/emoncms.conf')
-emoncsm_apikey = config.get(
-    'DEFAULT',     'emoncsm_apikey', fallback=emoncsm_apikey)
-emoncsm_url = config.get('DEFAULT',     'emoncsm_url',    fallback=emoncsm_url)
-emoncsm_node = config.get(
-    'Sensors',     'emoncsm_node',   fallback=emoncsm_node)
+emoncsm_apikey = config.get('DEFAULT',     'emoncsm_apikey', fallback = emoncsm_apikey)
+emoncsm_url    = config.get('DEFAULT',     'emoncsm_url',    fallback = emoncsm_url)
+emoncsm_node   = config.get('Sensors',     'emoncsm_node',   fallback = emoncsm_node)
 # ###########################
-
 
 def formatnumber(stringnumber, recalc):
     a_float = float(stringnumber)/recalc
     formatted_float = "{:.2f}".format(a_float)
     return formatted_float
 
-
-def sendData2webservice(datadict, node_name):
-    datajson = json.dumps(datadict)
-    # print(datajson)
-    myurl = '{}{}'.format(
-        emoncsm_url,
-        urlencode({'node': node_name, 'time': int(time.time()),
-                   'fulljson': datajson, 'apikey': emoncsm_apikey})
-    )
-    # print (myurl)
-    try:
-        r = requests.get(myurl)
-        # print (r)
-    except requests.exceptions.RequestException as e:  # This is the correct syntax
-        logging.error("Webservice Fehler URL:{} ERROR:{}".format(e, myurl))
-        time.sleep(5)
-    return
-
-
 # ###########################
 # Main
 # ###########################
 if __name__ == '__main__':
-    joinedRecord = dict()
+    # create logger
+    logger = logging.getLogger(__name__)
+    solar_logger.logger_setup('/home/pi/')
+    logger.info("Starting Internet Speed Test")
 
+    joinedRecord = dict()
+    EmonCMS = solar_threadhandler.DataLoggerQueue("InternetSpeedTest", emoncsm_url, emoncsm_apikey)
+    EmonCMS.StartQueue()
+ 
     try:
-        response = subprocess.run(
-            ['/usr/bin/speedtest-cli', '--json'], capture_output=True, encoding='utf-8')
+        response = subprocess.run(['/usr/local/bin/speedtest-cli', '--json'], capture_output=True, encoding='utf-8')
         # if speedtest-cli exited with no errors / ran successfully
         if response.returncode == 0:
             # from the csv man page
-            # "And while the module doesn’t directly support parsing strings, it can easily be done"
+            # "And while the module doesn't directly support parsing strings, it can easily be done"
             # this will remove quotes and spaces vs doing a string split on ','
             # csv.reader returns an iterator, so we turn that into a list
             # cols = list(csv.reader([response.stdout]))[0]
             speed = json.loads(response.stdout)
-
-            print(response.stdout)
+            # print (response.stdout)
             # turns 13.45 ping to 13
             ping = int(float((speed["ping"])))
 
             # speedtest-cli --csv returns speed in bits/s, convert to bytes
-            download = formatnumber(speed["download"], 1024*1024)
-            upload = formatnumber(speed["upload"], 1024*1024)
+            download = formatnumber(speed["download"],1024*1024)
+            upload = formatnumber(speed["upload"],1024*1024)
             joinedRecord.setdefault("Ping", ping)
             joinedRecord.setdefault("Download", download)
             joinedRecord.setdefault("Upload", upload)
             # print(joinedRecord)
-            sendData2webservice(joinedRecord, emoncsm_node)
+            # sendData2webservice(joinedRecord, emoncsm_node)
+            EmonCMS.addDataQueue(joinedRecord, emoncsm_node)
             joinedRecord.clear()
+            EmonCMS.StopQueue()
         else:
             print('speedtest-cli returned error: %s' % response.stderr)
     except KeyboardInterrupt:
         # Programm wird beendet wenn CTRL+C gedrückt wird.
-        print('Datensammlung wird beendet')
+        logger.info('Warte auf Ende von DataLoggerQueueProcessing')
+        EmonCMS.StopQueue()
+        logger.info('Datensammlung wird beendet')
     except Exception as e:
-        print(str(e))
+        logger.critical(f'Unerwarteter Abbruch: {str(e)}')
         sys.exit(1)
+    except (KeyboardInterrupt, SystemExit):
+        logger.info('Programm Abbruch wird eingeleitet')
     finally:
         # Das Programm wird hier beendet, sodass kein Fehler in die Console geschrieben wird.
-        # print('Programm wird beendet.')
+        EmonCMS.StopQueue()
+        logger.info('Programm wurde beendet.')
         sys.exit(0)
